06549a51c02c2e143c9a28409745ecfd265f0ba6
jay <jay.shadow7@gmail.com>
2019-12-31 18:13:26 +0800

    simple security init

--- simple-demo/src/main/java/cn/jay/simple/security/config/SecurityConfig.java
+++ simple-demo/src/main/java/cn/jay/simple/security/config/SecurityConfig.java
@@ -4,18 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,10 +29,14 @@ import javax.servlet.http.HttpServletResponse;
  * @Date: 2019/12/24 15:57
  */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -38,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("auth").password(passwordEncoder().encode("abc123")).authorities("add_user","update_user").roles("admin","user").build());
+        manager.createUser(User.withUsername("auth").password(passwordEncoder().encode("abc123")).authorities("add_user").build());
         return manager;
     }
 
@@ -62,11 +71,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().authorizeRequests().anyRequest().authenticated()
             .and().httpBasic()
             .and().formLogin()
                 .successHandler(authenticationSuccessHandler())
                 .failureHandler(authenticationFailureHandler())
+            .and().exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler())
             .and().csrf().disable()
 //            .addFilterBefore( , UsernamePasswordAuthenticationFilter.class)
         ;
@@ -75,7 +87,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private AuthenticationSuccessHandler authenticationSuccessHandler() {
         return (request, response, authentication) -> {
             response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            redisTemplate.opsForValue().set("authentication", authentication, 3600);
             response.getWriter().print(objectMapper.writeValueAsString(authentication));
+        };
+    }
+
+    private AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, exception) -> {
+            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().print(objectMapper.writeValueAsString(exception.getMessage()));
         };
     }
 
--- simple-demo/src/main/java/cn/jay/simple/security/controller/DemoController.java
+++ simple-demo/src/main/java/cn/jay/simple/security/controller/DemoController.java
@@ -1,5 +1,6 @@
 package cn.jay.simple.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping("/add")
+    @PreAuthorize("hasAuthority('add_user')")
     public String add() {
         return "/demo/add";
     }
 
     @GetMapping("/update")
+    @PreAuthorize("hasAuthority('update_user')")
     public String update() {
         return "/demo/update";
     }
