d3a823806ad03fc8398bd89ebda4a2a14c4aa458
jay <jay.shadow7@gmail.com>
2020-01-15 11:54:17 +0800

    oauth2 resource server 修改 todo

--- cloud-demo/oauth2-auth-server/src/main/java/cn/jay/cloud/oauth2/config/AuthorizationServerConfig.java
+++ cloud-demo/oauth2-auth-server/src/main/java/cn/jay/cloud/oauth2/config/AuthorizationServerConfig.java
@@ -1,10 +1,8 @@
 package cn.jay.cloud.oauth2.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
--- cloud-demo/oauth2-resource-server/src/main/java/cn/jay/cloud/oauth2/controller/DemoController.java
+++ cloud-demo/oauth2-resource-server/src/main/java/cn/jay/cloud/oauth2/controller/DemoController.java
@@ -2,8 +2,6 @@ package cn.jay.cloud.oauth2.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +19,7 @@ public class DemoController {
 
     @GetMapping("/add")
     @PreAuthorize("hasAuthority('add_user')")
-    public String add(@AuthenticationPrincipal OAuth2AccessToken token, HttpServletRequest request) {
+    public String add(HttpServletRequest request) {
         HttpSession session = request.getSession();
         return "#add_user# success =====by:";
     }
