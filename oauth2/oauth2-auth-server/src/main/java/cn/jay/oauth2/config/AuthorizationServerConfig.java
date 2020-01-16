fa1df8f97bf0fbe2ae31b1fbf56fb5f83cf9e89f
jay <jay.shadow7@gmail.com>
2020-01-16 16:01:34 +0800

    Authorization 服务器配置修改

--- oauth2/oauth2-auth-server/src/main/java/cn/jay/oauth2/config/AuthorizationServerConfig.java
+++ oauth2/oauth2-auth-server/src/main/java/cn/jay/oauth2/config/AuthorizationServerConfig.java
@@ -3,22 +3,13 @@ package cn.jay.oauth2.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.token.Token;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.TokenRequest;
-import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * @Author: Jay
@@ -29,16 +20,14 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-//    private final AuthorizationServerTokenServices tokenServices;
-
-    private final ClientDetailsService clientDetailsService;
+//    private final ClientDetailsService clientDetailsService;
 
     private final AuthenticationManager authenticationManager;
 
-    public AuthorizationServerConfig(ClientDetailsService clientDetailsService,
+    public AuthorizationServerConfig(
+//            ClientDetailsService clientDetailsService,
                                      AuthenticationManager authenticationManager) {
-//        this.tokenServices = tokenServices;
-        this.clientDetailsService = clientDetailsService;
+//        this.clientDetailsService = clientDetailsService;
         this.authenticationManager = authenticationManager;
         log.info("=======init AuthorizationServerConfig finish==========");
     }
@@ -51,7 +40,21 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.withClientDetails(clientDetailsService);
+//        clients.withClientDetails(clientDetailsService);
+        clients.inMemory()
+                .withClient("identity")
+                .secret(new BCryptPasswordEncoder().encode("123456"))
+                .authorizedGrantTypes("password", "authorization_code", "implicit")
+                .scopes("all")
+                .redirectUris("http://localhost:8080")
+                .autoApprove(false)
+                .and()
+                .withClient("client_book")
+                .secret(new BCryptPasswordEncoder().encode("123456"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("all")
+                .redirectUris("http://localhost:8080")
+                .autoApprove(false);
     }
 
     @Override
